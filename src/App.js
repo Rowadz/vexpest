41e6909b0bf72d3dbbfd09157467d1fe3196ef6a
Rowad <rowadwo@gmail.com>
2020-04-12 01:22:35 +0300

    home page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react'
 import './App.css'
+import { WiredTextarea } from 'wired-textarea'
+import { WiredButton } from 'wired-button'
 
 function App() {
-  return <section></section>
+  return (
+    <section className="center">
+      <div style={{ padding: '10px' }}>
+        <wired-textarea placeholder="Github username" rows="6"></wired-textarea>
+      </div>
+      <div style={{ padding: '10px', textAlign: 'center' }}>
+        <wired-button elevation="3">Get my dashbard</wired-button>
+      </div>
+    </section>
+  )
 }
 
 export default App
