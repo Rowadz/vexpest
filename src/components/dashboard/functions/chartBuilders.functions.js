63c362f19cf4599c5e5224f77705af7fb9ae4e0f
Rowad <rowadwo@gmail.com>
2020-04-12 20:09:49 +0300

    making the mapper shorter

--- src/components/dashboard/functions/chartBuilders.functions.js
+++ src/components/dashboard/functions/chartBuilders.functions.js
@@ -1,10 +1,8 @@
-const mapPieData = (data = [], y = 'stargazers_count', mapper) => {
-  console.log(mapper)
-  return data.map((obj) => ({
+const mapPieData = (data = [], y = 'stargazers_count', mapper) =>
+  data.map((obj) => ({
     name: obj.name,
     y: mapper ? mapper(obj[y]) : obj[y]
   }))
-}
 
 const mapWordcloudData = (data = []) =>
   data.map(({ watchers_count, name }) => ({ name, weight: watchers_count }))
